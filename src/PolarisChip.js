76874129b515c3255d4ac6ee42736e52cdfeb246
Oyindamola J Akanbi <ojakanbi@MacBook-Air-2.local>
2023-10-10 13:01:51 -0400

    psu chips w/ color and css checked

--- src/PolarisChip.js
+++ src/PolarisChip.js
@@ -3,24 +3,56 @@ import { LitElement, html, css } from 'lit';
 export class PolarisChip extends LitElement {
   static get properties() {
     return {
-      title: { type: String },
+      name: { type: String },
+      link: { type: String },
+      active: { type: Boolean, reflect: true },
     };
   }
 
   static get styles() {
     return css`
       :host {
-        display: block;
+        display: inline-block;
+        margin: 0 16px 16px 0;
       }
+
+      .link{ 
+        font-weight:bold;
+        padding: 8px 4px;
+        border: 2px solid #444;
+        color: #444;
+        font-size: 16px;
+        text-decoration: none;
+      }
+      .link:focus,
+      .link:hover,
+      :host([active]) .link{
+        background-color: #e4e5e7;
+        border: 2px solid #e4e5e7;
+        border-radius: 2px;
+        color: #005fa9;
+        cursor: pointer;
+        text-decoration: underline;
+      }
+
+      /* .link:hover {
+        background-color: #444;
+        color: #fff;
+        text-decoration: none;
+      } */
+
     `;
   }
 
   constructor() {
     super();
-    this.title = 'My boilerplate';
+    this.name = 'Football';
+    this.link = 'https://www.psu.edu/'
   }
 
   render() {
-    return html`<span>${this.title}</span>`;
+    return html`<a class='link' href="${this.link}"> 
+    <slot>${this.name}</slot>
+  </a>`;
   }
 }
